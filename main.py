2fe3c5abbbfdc0f6a55f4f2b9cd0d351ef6d34f9
menayimge87 <menayimge87@gmail.com>
2026-08-20 14:32:49 +0300

    Fix session expiration and prevent email deletion

--- main.py
+++ main.py
@@ -1168,6 +1168,7 @@ def delete_user(user_id):
     user = db.get_or_404(User, user_id)
     if user.email=='kedirmuhammed323@gmail.com':
         flash("who are you to do this? ")
+        return redirect(url_for("users"))
 
     if user.id == current_user.id:
         flash("You can't delete your own account while logged in.", "danger")
